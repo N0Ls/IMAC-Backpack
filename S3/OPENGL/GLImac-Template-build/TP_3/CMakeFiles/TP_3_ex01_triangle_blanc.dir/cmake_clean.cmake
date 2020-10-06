dbabe8c7be24c9b7cfe29733f0d01bc0dc2ed958
Nils <nilsland31@gmail.com>
2020-10-06 15:48:37 +0200

    Adding OPENGL

---
+++ S3/OPENGL/GLImac-Template-build/TP_3/CMakeFiles/TP_3_ex01_triangle_blanc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP_3_ex01_triangle_blanc.dir/ex01_triangle_blanc.cpp.o"
+  "TP_3_ex01_triangle_blanc"
+  "TP_3_ex01_triangle_blanc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP_3_ex01_triangle_blanc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
